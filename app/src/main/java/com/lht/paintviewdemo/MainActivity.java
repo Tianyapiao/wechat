eedb5b4e0640f5ffe56a8a1d3ad0118410310662
Tianyapiao <2499905061@qq.com>
2017-11-20 16:26:30 +0800

    paint3

---
+++ app/src/main/java/com/lht/paintviewdemo/MainActivity.java
@@ -0,0 +1,100 @@
+package com.lht.paintviewdemo;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
+
+import com.lht.paintviewdemo.view.CustomWebView;
+
+public class MainActivity extends AbstractActivity {
+
+    final static int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 0;
+
+    CustomWebView mWebView;
+    ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.webview_content_progress);
+
+        mWebView = (CustomWebView)findViewById(R.id.webview);
+        WebChromeClient WebChromeClient = new WebChromeClient() {
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+
+                if (newProgress == 100) {
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                    mProgressBar.setProgress(newProgress);
+                }
+            }
+        };
+        mWebView.setWebChromeClient(WebChromeClient);
+        //mWebView.loadUrl("http://www.google.com");
+        mWebView.loadUrl("http://www.w3school.com.cn/");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_share:
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+                }
+                else {
+                    PaintActivity.start(this, mWebView.captureWebViewVisibleSize(1f));
+                }
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        doNext(requestCode,grantResults);
+    }
+
+    private void doNext(int requestCode, int[] grantResults) {
+        if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                PaintActivity.start(this, mWebView.captureWebViewVisibleSize(1f));
+            } else {
+                // Permission Denied
+            }
+        }
+    }
+}
